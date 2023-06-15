d695fa6336cadadeeb6b16bc88d8c47da016c3b3
mrelcomt <mrelcomt@gmail.com>
2023-06-15 08:58:10 -0700

    print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - print one line using print f
+ * main - print a c program using printf
  *
- * Rwturn: Always 0
- * 
+ * Return: 0
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
