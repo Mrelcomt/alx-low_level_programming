4c3c29ec80a16350a6f73985da04eb89a69ecdf2
mrelcomt <mrelcomt@gmail.com>
2023-06-15 06:43:07 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-put "Programming is like building a multilingual puzzle \n"
+#include <stid.h>
+/**
+ * main - print a line of code using puts
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}	
