6ad62abdeb4fcc97a65843c23d3f80d69ecaaec5
mrelcomt <mrelcomt@gmail.com>
2023-06-15 07:25:37 -0700

    hello print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * the code print one line using print f
+ *
+ * on main function
+ * 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return(0);
+}
