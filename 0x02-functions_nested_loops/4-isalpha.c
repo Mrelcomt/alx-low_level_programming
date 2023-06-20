6dc4779425aa198e05787e760ba2659a18a76999
mrelcomt <mrelcomt@gmail.com>
2023-06-20 06:05:22 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
 /**
- * main - call function _is alpha
+ * _isalpha - checks for alphabetic character
+ * @c: the charcter to be checked
  *
- * _isalpha - return 1 if its alpha else 0
- *
- * Return: Always 0.
+ * Return: (0) if c is letter, (0)otherwise
  */
 int _isalpha(int c)
 {
 	int a;
 
-	if (c >= 65 || c <= 122)
+	if (c >= 65 && c <= 122)
 		a = 1;
 	else
 		a = 0;
 return (a);
-
-}
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	putchar(r + '0');
-	r = _isalpha('o');
-	putchar(r + '0');
-	r = _isalpha(108);
-	putchar(r + '0');
-	r = _isalpha(';');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
 }
