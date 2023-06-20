bf539ca9d6769b72b9894d248044a196c278dafe
mrelcomt <mrelcomt@gmail.com>
2023-06-20 02:45:44 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,19 +7,20 @@
  *
  */
 void print_alphabet(void);
-void print_alphabet(void)
-{
-	char c = 'a';
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-}
 int main(void)
 {
 	print_alphabet();
 	_putchar('\n');
 	return (0);
+}
+void print_alphabet(void)
+{
+        char c = 'a';
+
+        while (c <= 'z')
+        {
+                _putchar(c);
+                c++;
+        }
 }
