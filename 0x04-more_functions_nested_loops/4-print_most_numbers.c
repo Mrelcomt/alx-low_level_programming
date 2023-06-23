f8370aa0e152325469ed7244ce5c774360455529
mrelcomt <mrelcomt@gmail.com>
2023-06-23 13:56:51 -0700

    odd num

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - print most numbers
+ * Description: print odd numbers
+ * Return: there is no return
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i % 2 == 1)
+			_putchar(i);
+	}
+}
