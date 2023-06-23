0b681ad4c4f268e9b5ea1859880cc1a254821fce
mrelcomt <mrelcomt@gmail.com>
2023-06-23 13:15:54 -0700

    multiplie two numbers

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar(mul(98, 1024));
+	_putchar(mul(-402, 4096));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * mul - used to multiply two numbers
+ * @a: first num to multiply
+ * @b: second num to multiply
+ * Return: product of two numbers
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
