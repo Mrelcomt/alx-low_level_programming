599735ed0e818605da66f771074c171848746d72
mrelcomt <mrelcomt@gmail.com>
2023-06-23 12:58:53 -0700

    task 1 is digit

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	_putchar(_isdigit(c));
+	c = 'a';
+	_putchar(_isdigit(c));
+	return (0);
+}
