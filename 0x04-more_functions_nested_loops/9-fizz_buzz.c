808d2846fa02688f6860325d6d66ae4950b295bb
mrelcomt <mrelcomt@gmail.com>
2023-06-24 11:30:13 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * fizz_buzz - print fizz buzz on multiplier of 3 and 5
+ * Return: there is no return
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf(" Fizz Buzz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else
+			printf(" %d",i);
+	}
+}
+/**
+ * main - call funcuiton fizz_buzz
+ * Return: always 0
+ */
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
