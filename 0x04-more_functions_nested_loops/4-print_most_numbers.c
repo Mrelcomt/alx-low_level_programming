7021eaf8ac57beb41d5fc170a6bd4a9cdd2ebfea
mrelcomt <mrelcomt@gmail.com>
2023-06-23 23:03:19 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,7 +11,14 @@ void print_most_numbers(void)
 
 	for (i = 48; i <= 57; i++)
 	{
-		if (i < 50 && i % 2 == 1)
+		if (i < 50)
+		{
+			if (i % 2 == 1)
+			{
+				_putchar(i);
+			}
+		}
+		else
 			_putchar(i);
 	}
 }
