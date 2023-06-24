27d893623c467f8b6058f6d165545d44c0e75c6c
mrelcomt <mrelcomt@gmail.com>
2023-06-24 10:18:08 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,13 +12,21 @@ void print_square(int n)
 	int j;
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n == 0)
 	{
-		for (j = 0; j < n; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
+	}
+	else
+	{
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 
 }
