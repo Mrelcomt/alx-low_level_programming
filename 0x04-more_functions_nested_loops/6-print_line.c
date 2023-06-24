169a6654ac744addfac90f0cd52a547c83eafded
mrelcomt <mrelcomt@gmail.com>
2023-06-24 07:41:54 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - will print line
+ * @n: number recived from main
+ * Description: it prints line by accepting numbers
+ * Return: there is no return
+ */
+void print_line(int n)
+{
+
+	int j;
+
+
+
+	for (j = n; j > 0; j--)
+	{
+		_putchar('_');
+	}
+	_putchar('$');
+	_putchar('\n');
+
+}
