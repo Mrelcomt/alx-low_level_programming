8bdc3089a86448150c6ec4a6a4f3cf14d71f5843
mrelcomt <mrelcomt@gmail.com>
2023-06-24 18:14:37 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ void fizz_buzz(void)
 	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf(" Fizz Buzz");
+			printf(" FizzBuzz");
 		else if (i % 5 == 0)
 			printf(" Buzz");
 		else if (i % 3 == 0)
