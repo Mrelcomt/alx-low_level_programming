6d06a6c6715074c274ccb4e6aef1c21f1f646724
mrelcomt <mrelcomt@gmail.com>
2023-06-26 01:34:43 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset value of n to 98
+ * @n: is an address which gone reset
+ * Return: the new value on address
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
