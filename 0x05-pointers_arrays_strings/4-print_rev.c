5902b5997fd3ca79a771e66d81c1f182ba255b9e
mrelcomt <mrelcomt@gmail.com>
2023-06-27 02:45:39 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,22 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * rev_string - used to reverse the string
- * @s: the given string
- * Return: there is no return
+ * print_rev - prints a string in reverse
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
  */
 void print_rev(char *s)
 {
-	int l, i;
-	char ch;
+	int index;
 
-	/*find string length without null char*/
-	for (l = 0; s[l] != '\0'; ++l)
+	/*finds the length of string without null character*/
+	for (index = 0; s[index] != '\0'; ++index)
 		;
 
-	/*swap the string by looping to half the string*/
-	for (i = 0; i < l / 2; ++i)
-	{
-		ch = s[i];
-		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
-		s[l - 1 - i] = ch;
-	}
+	/*print char from the last index as you decrement*/
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+	_putchar('\n');
 }
