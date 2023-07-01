e725b238a4ddae143656d3b1f8a672816f5cf52a
mrelcomt <mrelcomt@gmail.com>
2023-07-01 00:03:42 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	_putchar((n1 % 10) + '0');
+}
