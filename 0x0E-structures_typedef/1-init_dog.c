58d2be9b2dacc6262c3faaa2147bb835e356aaff
mrelcomt <mrelcomt@gmail.com>
2023-07-17 11:25:19 -0700

    initialize

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,10 +1,19 @@
 #include "dog.h"
+/**
+ * init_dog - initialize dog struct
+ *
+ * d: pased struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner
+ * Return: there is no return
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
+	/* member of structure*/
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
-	
-	
+
+
 }
