c390a209f400e35c1576c4f1fe02f4e45d813d68
mrelcomt <mrelcomt@gmail.com>
2023-07-17 04:49:51 -0700

    struct edit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+/**
+ * dog - structure with different attribute
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: whose owne the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
- };
+};
+#endif
